81427d73357d1632f156e748095406b7b11f1dfe
Matheus de Souza <mws.souza@gmail.com>
2012-03-09 11:02:06 -0300

    Enviando cenas SelectLanguage, SelectKitchen, Wall Builder e Make Your Kitchen-NGUI. Com NGUI funcionando em todas as telas. Porém, na tela MakeYourKitchen-NGUI só o panel da camera está funcionando os outros panels estão em andamento.

---
+++ Assets/Custom Assets/Scripts/GUI/New Gui/MakeYourKitchen/Panel Catalog/CatalogController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class CatalogController : MonoBehaviour
+{
+
+	private bool IsShowingCategories;
+	public void ShowCategories()
+	{
+		
+	}
+}

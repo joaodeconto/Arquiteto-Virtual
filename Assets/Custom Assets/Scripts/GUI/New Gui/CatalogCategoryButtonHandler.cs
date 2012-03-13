6191a82a88d955218370fb10c8d44ac4e867925f
Crisitan Ritter <cristiansrguitar@hotmail.com>
2012-03-13 15:31:49 -0300

    Enviando para receber scripts do Mr. Matheus
    Signed-off-by: Crisitan Ritter <cristiansrguitar@hotmail.com>

--- Assets/Custom Assets/Scripts/GUI/New Gui/CatalogCategoryButtonHandler.cs
+++ Assets/Custom Assets/Scripts/GUI/New Gui/CatalogCategoryButtonHandler.cs
@@ -6,6 +6,7 @@ public class CatalogCategoryButtonHandler : MonoBehaviour {
 	public GameObject item;
 	public Camera cameraTarget;
 	public Transform rootForBounds;
+	public NTweener tween;
 	
 	// Use this for initialization
 	void Start () {
@@ -24,6 +25,9 @@ public class CatalogCategoryButtonHandler : MonoBehaviour {
 			newItem.transform.parent = transform;
 			newItem.transform.localPosition = new Vector3(0, (i * -185), 0);
 			newItem.transform.localScale = item.transform.localScale;
+			newItem.AddComponent<TweenPlayerButton>();
+			newItem.GetComponent<TweenPlayerButton>().parallelTweens = new NTweener[1];
+			newItem.GetComponent<TweenPlayerButton>().parallelTweens[0] = tween;
 			foreach (UISprite sprite in newItem.GetComponentsInChildren<UISprite>()) {
 				if (sprite.name.Equals("UISprite")) {
 					sprite.spriteName = Line.CurrentLine.categories[i].ImageReference;

6ea04fb24c087a11160a89cb64fef86c3904c567
Crisitan Ritter <cristiansrguitar@hotmail.com>
2012-03-13 15:20:11 -0300

    Enviando minhas modificacoes
    Signed-off-by: Crisitan Ritter <cristiansrguitar@hotmail.com>

--- Assets/Custom Assets/Scripts/GUI/New Gui/CatalogCategoryButtonHandler.cs
+++ Assets/Custom Assets/Scripts/GUI/New Gui/CatalogCategoryButtonHandler.cs
@@ -3,23 +3,33 @@ using System.Collections;
 
 public class CatalogCategoryButtonHandler : MonoBehaviour {
 	
-	//public UIAtlas atlas;
+	public GameObject item;
+	public Camera cameraTarget;
+	public Transform rootForBounds;
+	
 	// Use this for initialization
 	void Start () {
 		Invoke("CatalogCategory", 0.1f);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-	
 	void CatalogCategory () {
+		int i = 0;
 		foreach (Category category in Line.CurrentLine.categories) {
-			UISprite newSprite = new UISprite();
-			newSprite.texture = category.Image;
-			newSprite.spriteName = category.Name;
-//			atlas.sprites.Add(newSprite);
+			GameObject newItem = Instantiate(item) as GameObject;
+			newItem.name = item.name;
+			newItem.name += " " + i;
+			newItem.GetComponent<TooltipHandler>().SetTooltip(Line.CurrentLine.categories[i].Name);
+			newItem.GetComponent<UIDragCamera>().target = cameraTarget;
+			newItem.GetComponent<UIDragCamera>().rootForBounds = rootForBounds;
+			newItem.transform.parent = transform;
+			newItem.transform.localPosition = new Vector3(0, (i * -185), 0);
+			newItem.transform.localScale = item.transform.localScale;
+			foreach (UISprite sprite in newItem.GetComponentsInChildren<UISprite>()) {
+				if (sprite.name.Equals("UISprite")) {
+					sprite.spriteName = Line.CurrentLine.categories[i].ImageReference;
+				}
+			}
+			++i;
 		}
 	}
 }
--- Assets/Custom Assets/Scripts/Initialization/Initialization.cs
+++ Assets/Custom Assets/Scripts/Initialization/Initialization.cs
@@ -58,7 +58,7 @@ public class Initialization : MonoBehaviour {
 			foreach (Transform mobile in categoriesTransform.transform) {
 				furniture.Add(mobile.gameObject);
 			}
-			categories.Add(new Category(categoriesTransform.name,furniture,categoriesTransform.GetComponent<MakeCategory>().ico));
+			categories.Add(new Category(categoriesTransform.name,furniture,categoriesTransform.GetComponent<MakeCategory>().imageReference));
 		}
 		
 		print("categories.Count: " + categories.Count);

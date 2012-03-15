65f711fdcd4d9356d2d7c04af6e89e115ced403f
Crisitan Ritter <cristiansrguitar@hotmail.com>
2012-03-15 10:17:56 -0300

    Enviando Rotação do sol e Tooltip ajustado

--- Assets/Custom Assets/Scripts/GUI/New Gui/Editor/TooltipHandlerEditor.cs
+++ Assets/Custom Assets/Scripts/GUI/New Gui/Editor/TooltipHandlerEditor.cs
@@ -28,61 +28,70 @@ public class TooltipHandlerEditor : Editor
 		
 		if (!tooltipHandler.getViaCode)
 		{
-			GUILayout.Label ("GameObject Reference:");
-			tooltipHandler.gameObject = EditorGUILayout.ObjectField (tooltipHandler.gameObject, typeof(GameObject)) as GameObject;
+			tooltipHandler.getViaLabel = EditorGUILayout.Toggle ("Get Via Label?", tooltipHandler.getViaLabel);
 			GUILayout.Space (5f);
-			
-			if (tooltipHandler.gameObject != null)
-			{
-				Component[] comps = tooltipHandler.gameObject.GetComponents (typeof(Component));
-				tooltipHandler.components = new List<Type> ();
-				foreach (Component c in comps)
-				{
-					if (c.GetType ().GetFields ().Length != 0) {
-						tooltipHandler.components.Add (c.GetType ());
-					}
-				}
-				if (tooltipHandler.components.Count == 0 ||
-					tooltipHandler.selectedComp > tooltipHandler.components.Count)
-				{
-					tooltipHandler.selectedComp = 0;
-					return;
-				}
-				
-				GUILayout.Label ("Components:");
-				tooltipHandler.selectedComp = EditorGUILayout.Popup (tooltipHandler.selectedComp, ComponentsNames ());
+			if (!tooltipHandler.getViaLabel) {
+				GUILayout.Label ("GameObject Reference:");
+				tooltipHandler.gameObject = EditorGUILayout.ObjectField (tooltipHandler.gameObject, typeof(GameObject)) as GameObject;
 				GUILayout.Space (5f);
 				
-				if (tooltipHandler.selectedComp != 0)
+				if (tooltipHandler.gameObject != null)
 				{
-					tooltipHandler.vars = new List<FieldInfo> ();
-					string type = "";
-					foreach (FieldInfo f in tooltipHandler.components[tooltipHandler.selectedComp-1].GetFields())
+					Component[] comps = tooltipHandler.gameObject.GetComponents (typeof(Component));
+					tooltipHandler.components = new List<Type> ();
+					foreach (Component c in comps)
 					{
-						if (f.FieldType == type.GetType ())
-						{
-							tooltipHandler.vars.Add (f);
+						if (c.GetType ().GetFields ().Length != 0 &&
+							c.GetType ().ToString() != "TooltipHandler") {
+							tooltipHandler.components.Add (c.GetType ());
 						}
 					}
-					
-					if (tooltipHandler.vars.Count == 0 ||
-						tooltipHandler.selectedVar > tooltipHandler.vars.Count)
+					if (tooltipHandler.components.Count == 0 ||
+						tooltipHandler.selectedComp > tooltipHandler.components.Count)
 					{
-						tooltipHandler.selectedVar = 0;
+						tooltipHandler.selectedComp = 0;
 						return;
 					}
 					
-					GUILayout.Label ("String Variables:");
-					tooltipHandler.selectedVar = EditorGUILayout.Popup (tooltipHandler.selectedVar, VarsNames ());
+					GUILayout.Label ("Components:");
+					tooltipHandler.selectedComp = EditorGUILayout.Popup (tooltipHandler.selectedComp, ComponentsNames ());
 					GUILayout.Space (5f);
-					if (tooltipHandler.selectedVar != 0)
+					
+					if (tooltipHandler.selectedComp != 0)
 					{
-						string val = tooltipHandler.vars [tooltipHandler.selectedVar - 1].GetValue (
-										tooltipHandler.gameObject.GetComponent (
-										tooltipHandler.components [tooltipHandler.selectedComp - 1])).ToString ();
-						tooltipHandler.label = val;
+						tooltipHandler.vars = new List<FieldInfo> ();
+						string type = "";
+						foreach (FieldInfo f in tooltipHandler.components[tooltipHandler.selectedComp-1].GetFields())
+						{
+							if (f.FieldType == type.GetType ())
+							{
+								tooltipHandler.vars.Add (f);
+							}
+						}
+						
+						if (tooltipHandler.vars.Count == 0 ||
+							tooltipHandler.selectedVar > tooltipHandler.vars.Count)
+						{
+							tooltipHandler.selectedVar = 0;
+							return;
+						}
+						
+						GUILayout.Label ("String Variables:");
+						tooltipHandler.selectedVar = EditorGUILayout.Popup (tooltipHandler.selectedVar, VarsNames ());
+						GUILayout.Space (5f);
+						if (tooltipHandler.selectedVar != 0)
+						{
+							string val = tooltipHandler.vars [tooltipHandler.selectedVar - 1].GetValue (
+											tooltipHandler.gameObject.GetComponent (
+											tooltipHandler.components [tooltipHandler.selectedComp - 1])).ToString ();
+							tooltipHandler.label = val;
+						}
 					}
 				}
+			} else {
+			GUILayout.Label ("Digit a label to appear in tooltip:");
+			tooltipHandler.label = GUILayout.TextField(tooltipHandler.label);
+			GUILayout.Space (5f);
 			}
 		} else {
 			GUILayout.Label ("Get Via Code with this method:\n" +

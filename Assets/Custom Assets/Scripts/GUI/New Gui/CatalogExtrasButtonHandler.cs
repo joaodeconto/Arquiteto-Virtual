1ff6aef8f7ff4b82ff6a12475854c980c478cf19
Crisitan Ritter <cristiansrguitar@hotmail.com>
2012-03-21 10:29:31 -0300

    Enviando cena montada para Extras e ajuste do prefab Item

---
+++ Assets/Custom Assets/Scripts/GUI/New Gui/CatalogExtrasButtonHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections;
+
+public class CatalogExtrasButtonHandler : MonoBehaviour {
+	
+	public GameObject item;
+	public Camera cameraTarget;
+	public Transform rootForBounds;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

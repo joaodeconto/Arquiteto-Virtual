6a87ce1b70129b290e7fb120b5d2fd1ab05021c2
Joao de Conto <blackbugio@hotmail.com>
2012-03-21 12:03:54 -0300

    Enviando troca de portas

--- Assets/Custom Assets/Scripts/GUI/New Gui/MakeYourKitchen/Panel Info/CheckBoxDoorSideHandler.cs
+++ Assets/Custom Assets/Scripts/GUI/New Gui/MakeYourKitchen/Panel Info/CheckBoxDoorSideHandler.cs
@@ -11,6 +11,15 @@ public class CheckBoxDoorSideHandler : MonoBehaviour {
 	
 	public DoorSideEnum doorSideEnum;
 	
+	private InfoController infoController;
+	private Camera3d camera3d;
+	
+	void Awake ()
+	{
+		infoController = GameObject.FindWithTag("GameController").GetComponentInChildren<InfoController>();
+		camera3d = GameObject.FindWithTag("MainCamera").GetComponent<Camera3d>();
+	}
+	
 	void OnClick ()
 	{
 		bool success = false;
@@ -21,7 +30,15 @@ public class CheckBoxDoorSideHandler : MonoBehaviour {
 			Debug.LogError ("Não foi possível encontrar o móvel selecionado!");
 		}
 					
-		selectedModule.GetComponent<InformacoesMovel>().ToogleDoorSide();	
+		GameObject newModule = selectedModule.GetComponent<InformacoesMovel>().ToggleDoorSide();
+				
+		newModule.GetComponent<SnapBehaviour>().Select = true;
+		newModule.GetComponent<Rigidbody>().collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+		camera3d.GetComponent<RenderBounds>().Display = true;
+		camera3d.GetComponent<RenderBounds>().SetBox(newModule);
+		camera3d.GetComponent<RenderBounds>().UpdateObj();
+		
+		infoController.SendMessage("UpdateInfo", newModule.GetComponent<InformacoesMovel>());
 		
 	}
 }

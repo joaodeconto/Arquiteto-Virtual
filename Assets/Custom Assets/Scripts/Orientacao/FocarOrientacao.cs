71548c3814646e665824256be8ad5fb0ac2df6c5
Matheus de Souza <mws.souza@gmail.com>
2012-03-26 15:10:53 -0300

    Rotação Cubo funcionando só falta verificar se posições estão corretas

--- Assets/Custom Assets/Scripts/Orientacao/FocarOrientacao.cs
+++ Assets/Custom Assets/Scripts/Orientacao/FocarOrientacao.cs
@@ -8,124 +8,84 @@ public enum Orientacao {
 public class FocarOrientacao : MonoBehaviour {
 	
 	public Orientacao orientacao = Orientacao.Frente;
-	private Vector3 posicao = Vector3.zero;
-	private GameObject camera;
-	
 	public float minimumHeight = 2;
+	public float tweenTime = 2.0f;
 	
+	private float distanceRate = 1.5f;
+	private float cameraInclination = 11f;
+	
+	private Vector3 newDirection = Vector3.zero;
+	private Vector3 newPosition = Vector3.zero;
+	private float distanceFromMiddle = 0f; 
+	
+	private GameObject mainCamera;
+				
 	void OnMouseUp () {
 		
-		camera = GameObject.Find("Main Camera");
+		mainCamera = GameObject.FindWithTag("MainCamera");
 		
-		if (camera == null)
+		if (mainCamera == null)
 		{
 			Debug.LogError ("Main camera can't be found");
 			return;
 		}
 		
-		if (orientacao != Orientacao.Perspectiva) {
-		
-			Debug.LogError ("orientacao: " + orientacao.ToString());
-		
-			if (orientacao == Orientacao.Frente){
-				if (camera.transform.eulerAngles.y > 180)
-					posicao = new Vector3(0, 360, 0);	
-				else
-					posicao = new Vector3(0, 0, 0);
-			}
-			
-			if (orientacao == Orientacao.Atras)
-				posicao = new Vector3(0, 180, 0);
-			
-			if (orientacao == Orientacao.Direita){
-				if (camera.transform.eulerAngles.y < 180)
-					posicao = new Vector3(0, -90, 0);
-				else
-					posicao = new Vector3(0, 270, 0);
-			}
-			if (orientacao == Orientacao.Esquerda)
-				posicao = new Vector3(0, 90, 0);
-			if (orientacao == Orientacao.Cima)
-				posicao = new Vector3(90, 0, 0);
-			if (orientacao == Orientacao.Baixo)
-				posicao = new Vector3(-90, 0, 0);
-			
-			StartCoroutine(LerpCamera(camera.transform.localEulerAngles, posicao));
-			
-		} else {
-			posicao = Vector3.zero;
-		 	StartCoroutine(LookLerpCamera(camera.transform.position, posicao));
+		GameObject[] phantomFloors = GameObject.FindGameObjectsWithTag ("ChaoVazio");
+		if (phantomFloors == null || phantomFloors.Length == 0)
+		{
+			Debug.LogError ("Não foi possível encontrar nenhum \"ChaoVazio\" por favor verifique se as cenas estão interligadas corretamente.");
+			return;	
 		}
+		
+		Debug.LogWarning ("distanceFromMiddle: " + distanceFromMiddle);
+		Debug.LogWarning ("Mathf.Sqrt(distanceRate): " + Mathf.Sqrt (distanceRate));
+		distanceFromMiddle = phantomFloors.Length * distanceRate;
+		distanceFromMiddle /= Mathf.Sqrt(distanceFromMiddle); 
+		Debug.LogWarning ("distanceFromMiddle: " + distanceFromMiddle);
+		
+		Debug.LogError ("orientacao: " + orientacao.ToString());
+		newDirection   = Vector3.zero;
+		newDirection.x = cameraInclination;
+		newPosition    = WallBuilder.ROOT;
+		newPosition.y  = 1.7f;
+		switch (orientacao)
+		{
+			case Orientacao.Frente:
+				newDirection.y = 0f;
+				newPosition.z -= distanceFromMiddle;
+				break;
+			case Orientacao.Direita:
+				newDirection.y = 270f;
+				newPosition.x += distanceFromMiddle;
+				break;
+			case Orientacao.Atras:
+				newDirection.y = 180f;
+				newPosition.z += distanceFromMiddle;
+				break;
+			case Orientacao.Esquerda:
+				newDirection.y  = 90f;
+				newPosition.x -= distanceFromMiddle;
+				break;
+			case Orientacao.Cima:
+				newDirection.x  = 90;
+				newPosition.y += distanceFromMiddle;
+				break;
+			case Orientacao.Baixo:
+				newDirection.x  = -90;
+				newPosition.y -= distanceFromMiddle;
+				break;
+			case Orientacao.Perspectiva:
+				newDirection   = new Vector3(45f,45f,0f);
+				newPosition.x -= distanceFromMiddle;
+				newPosition.y += distanceFromMiddle;
+				newPosition.z -= distanceFromMiddle;
+				break;
+		}
+		
+		iTween.RotateTo (mainCamera, iTween.Hash (iT.RotateTo.rotation, newDirection,
+												iT.RotateTo.time, tweenTime));
+		
+		iTween.MoveTo (mainCamera, iTween.Hash (iT.MoveTo.position, newPosition,
+												iT.MoveTo.time, tweenTime));
 	}
-	
-	IEnumerator LerpCamera (Vector3 start, Vector3 end){
-	
-		Transform groundTransform = GameObject.Find("ParentChao").transform;
-		Vector3 middleOfGround = Vector3.zero;
-		
-		float i = 0;
-		
-		for(i = groundTransform.childCount - 1; i != -1; --i){
-			middleOfGround += groundTransform.GetChild((int)i).position;
-		}
-		
-		Quaternion bkpRotation = camera.transform.rotation;
-		
-		middleOfGround /= i;
-		//monkeys gonna patch...
-		middleOfGround += WallBuilder.ROOT;
-		camera.transform.localEulerAngles = end;
-		middleOfGround -= camera.transform.forward * Vector3.Distance(camera.transform.position, middleOfGround);
-		camera.transform.rotation = bkpRotation;
-		
-		if(middleOfGround.y < minimumHeight){
-			middleOfGround.y = minimumHeight;
-		}
-		
-		Vector3 lastPosition = camera.transform.position;
-		
-		i = 0;
-	    while(i < 1) {
-			//camera.transform.LookAt(middleOfGround);//,end,i);
-	    	camera.transform.localEulerAngles = Vector3.Lerp(start, end, i * 2f);
-	        camera.transform.position		  = Vector3.Lerp(lastPosition, middleOfGround, i * 2f);
-	        i += Time.deltaTime;
-	        yield return null;
-	    }
-	}
-	
-	IEnumerator LookLerpCamera (Vector3 start, Vector3 end){
-	
-		Transform groundTransform = GameObject.Find("ParentChao").transform;
-		Vector3 middleOfGround = Vector3.zero;
-		
-		float i = 0;
-		
-		for(i = groundTransform.childCount - 1; i != -1; --i){
-			middleOfGround += groundTransform.GetChild((int)i).position;
-		}
-		
-		Quaternion bkpRotation = camera.transform.rotation;
-		
-		middleOfGround /= i;
-		camera.transform.localEulerAngles = end;
-		middleOfGround -= camera.transform.forward * Vector3.Distance (camera.transform.position, middleOfGround);
-		camera.transform.rotation = bkpRotation;
-		
-		
-		if(middleOfGround.y < minimumHeight){
-			middleOfGround.y = minimumHeight;
-		}
-		
-		Vector3 lastPosition = camera.transform.position;
-		
-	    while(i < 1) {
-			Quaternion rotation = Quaternion.LookRotation(end - start);
-			camera.transform.rotation = Quaternion.Slerp(camera.transform.rotation, rotation, i * 2f);
-	        camera.transform.position = Vector3.Lerp(lastPosition, middleOfGround, i * 2f);
-	        i += Time.deltaTime;
-	        yield return null;
-	    }
-	}
-
 }

034d0565e93c6e9d239059f96cb3d3a7f8f9fae1
Matheus de Souza <mws.souza@gmail.com>
2012-05-11 15:33:22 -0300

    Rev SnapBehaviour
    Revertendo mudanças no snap por que o novo ficou muito ruim.

--- Assets/Custom Assets/Scripts/Moveis/Movement/SnapBehaviour.cs
+++ Assets/Custom Assets/Scripts/Moveis/Movement/SnapBehaviour.cs
@@ -1,97 +1,107 @@
-#define DEEP_DEBUG
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-[RequireComponent (typeof(Rigidbody))]
-public class SnapBehaviour : MonoBehaviour {
+public class SnapBehaviour : MonoBehaviour
+{
 
-	const float IPSLON = 0.2f;
+	#region public variables
+	public bool canSnapTop;
+	public bool canSnapBottom;
+	#endregion
+
+	public bool collidedWithWall = false;
 
 	public bool wasDragged { get; private set; }
 
+	private bool isSelected = false;
 	private Camera mainCamera;
 
 	#region temp variables
-	private Ray ray;
-	private RaycastHit hit;
-	private Vector3 lastMousePosition;
-	private Vector3 mousePosition;
+	private Vector3 direction;
+	private GameObject collidingMobile;
 	#endregion
 
-	private float coldDownTime;
+	private float coolDownTime;
 	private bool enableDrag;
-	private bool isSelected;
-
-	private Vector3 referencePoint;
-	private Transform referenceTransform;
-	private float halfModuleWidth;
+	private Vector3 p; //Pivot value -1..1, calculated from Mesh bounds
+	private Vector3 last_p; //Last used pivot
 	private int scenarioLayer;
 	#region Unity Methods
 
-	private bool isOnTheWall;
-	void Start()
+	public static void ActivateAll ()
 	{
-		coldDownTime = 0.4f;
-		enableDrag = false;
-        rigidbody.detectCollisions = true;
-        rigidbody.mass = 1000.0f;
-        rigidbody.drag = 100.0f;
-        rigidbody.angularDrag = 100.0f;
-        rigidbody.freezeRotation = true;
-
-		mainCamera = GameObject.FindWithTag ("MainCamera").camera;
-		halfModuleWidth = (collider as BoxCollider).size.x / 1.9f;
-		scenarioLayer = 1 << LayerMask.NameToLayer("Cenario");
-		isOnTheWall = false;
+		SetAll (true);
 	}
 
-	// Android & Iphone Method to move object
-	void Update ()
+	public static void DeactivateAll ()
 	{
-		//se nada aconteceu ou não foi clicado no objeto retorna
-#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_WEBPLAYER || UNITY_EDITOR
-		if (Input.GetMouseButtonDown(0) ||
-			Input.GetMouseButton(0) 	||
-			Input.GetMouseButtonUp(0))
-		{
-			lastMousePosition = mousePosition;
-			mousePosition 	  = Input.mousePosition;
-#else
-			if (Input.GetTouch(0).phase == )
-			{
-				Touch touch = Input.GetTouch(0);
-				Vector2 mousePosition = touch.position;
-				//TODO this works
-#endif
-//	        ray = mainCamera.ScreenPointToRay(mousePosition);
-//
-//	        Physics.Raycast(ray, out hit, Mathf.Infinity);
-//
-//	        if (hit.transform != transform)
-//	        	return;
-		}
-		else
-		{
-			return;
+		SetAll (false);
+	}
+
+	private static void SetAll (bool value)
+	{
+		GameObject[] furniture = GameObject.FindGameObjectsWithTag ("Movel");
+		foreach (GameObject mobile in furniture) {
+			mobile.GetComponent<SnapBehaviour> ().enabled = value;
 		}
 
-#if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX || UNITY_WEBPLAYER || UNITY_EDITOR
+		GameObject activeMobile = GameObject.FindGameObjectWithTag ("MovelSelecionado");
+		if (activeMobile != null) {
+			activeMobile.GetComponent<SnapBehaviour> ().enabled = value;
+		}
+	}
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			SelectObject();
+	void Start ()
+	{
+		coolDownTime = 0.4f;
+		enableDrag = false;
+		rigidbody.detectCollisions = true;
+		rigidbody.mass = 1000.0f;
+		rigidbody.drag = 100.0f;
+		rigidbody.angularDrag = 100.0f;
+		rigidbody.freezeRotation = true;
+		scenarioLayer = 1 << LayerMask.NameToLayer ("Cenario");
+	}
+
+	#endregion
+
+	public bool Select {
+		get { return isSelected; }
+		set {
+			//print("setado: " + this.name + " para " + (value == true? "selecionado" : "Não selecionado"));
+			isSelected = value;
+			if (isSelected) {
+				Invoke ("EnableDrag", coolDownTime);
+			}
 		}
- 		else if (Input.GetMouseButton(0) &&
-				 Vector3.Distance(lastMousePosition, Input.mousePosition) > IPSLON)
-		{
-			DragObject();
-		}
-		else if (Input.GetMouseButtonUp(0))
-		{
-			DropObject();
-		}
-#else
+	}
+    
+	private void EnableDrag ()
+	{
+		enableDrag = true;
+	}
+
+	#if (!UNITY_ANDROID && !UNITY_IPHONE) || UNITY_EDITOR
+	void OnMouseDown ()
+	{
+		SelectObject ();
+	}
+
+	void OnMouseDrag ()
+	{
+		DragObject ();
+	}
+
+	void OnMouseUp ()
+	{
+		DropObject ();
+	}
+	#endif
+
+	#if UNITY_ANDROID || UNITY_IPHONE
+	// Android & Iphone Method to move object
+	void Update () {
 		if (Input.touchCount == 1) {
 			Touch touch = Input.GetTouch(0);
 			switch (touch.phase) {
@@ -109,323 +119,193 @@ public class SnapBehaviour : MonoBehaviour {
 					break;
 			}
 		}
-#endif
 	}
-
-	#endregion
-
-	void OnCollisionEnter(Collision collision)
-	{
-		if(!isOnTheWall)
-		{
-			if ( (collision.transform.tag == "Parede"))
-			{
-				transform.rotation = collision.transform.rotation;
-			}
-			else if ( hit.transform.tag == "Teto" &&
-					 (GetComponent<InformacoesMovel> ().tipoMovel == TipoMovel.MOVEL))
-			{
-				Vector3 nEuler = hit.transform.eulerAngles;
-				nEuler.x = - nEuler.x;
-				transform.eulerAngles = nEuler;
-			}
-
-			isOnTheWall = true;
-		}
-	}
-
-	public static void ActivateAll()
-	{
-		SetAll(true);
-	}
-
-	public static void DeactivateAll()
-	{
-		SetAll(false);
-	}
-
-	private static void SetAll(bool value)
-	{
-		GameObject[] furniture = GameObject.FindGameObjectsWithTag("Movel");
-		foreach(GameObject mobile in furniture)
-		{
-			mobile.GetComponent<SnapBehaviour>().enabled = value;
-		}
-
-		GameObject activeMobile = GameObject.FindGameObjectWithTag("MovelSelecionado");
-		if(activeMobile != null)
-		{
-			activeMobile.GetComponent<SnapBehaviour>().enabled = value;
-		}
-	}
-
-    public bool Select {
-        get { return isSelected; }
-        set {
-			//print("setado: " + this.name + " para " + (value == true? "selecionado" : "Não selecionado"));
-		    isSelected = value;
-		    if(isSelected)
-		    {
-		    	Invoke("EnableDrag", coldDownTime);
-		    }
-        }
-    }
-
-    private void EnableDrag()
-    {
-    	enableDrag = true;
-    }
+	#endif
 
 	void SelectObject ()
 	{
-        ray = mainCamera.ScreenPointToRay(mousePosition);
+		if (!enabled)
+			return;
 
-        if (!Physics.Raycast(ray, out hit, Mathf.Infinity, scenarioLayer))
-        {
-        	return;
-        }
+		GameObject[] furniture = GameObject.FindGameObjectsWithTag ("Movel");
 
-		GameObject[] furniture 	   = GameObject.FindGameObjectsWithTag("Movel");
+		if (furniture.Length == 0)
+			return;
+
+		for (int i = 0; i != furniture.Length; ++i) {
+			furniture [i].rigidbody.constraints = RigidbodyConstraints.FreezeAll;
+		}
+
 		GameObject activeFurniture = GameObject.FindGameObjectWithTag ("MovelSelecionado");
 
-		if(furniture.Length == 0 && activeFurniture == null)
-			return;
-
-		for(int i = 0; i != furniture.Length; ++i)
-		{
-			furniture[i].rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-		}
-
-		if(activeFurniture != null)
-		{
+		if (activeFurniture != null)
 			activeFurniture.rigidbody.constraints = RigidbodyConstraints.FreezeAll;
-		}
 
-		if (referenceTransform == hit.transform)
-		{
-			referencePoint = hit.point;
-		}
-		else
-		{
-			referencePoint = transform.position;
-		}
-
-		referenceTransform = hit.transform;
-
-		if (hit.transform.tag == "Parede")
-		{
-			transform.rotation = hit.transform.rotation;
-		}
-		else if ( hit.transform.tag == "Teto" &&
-				 (GetComponent<InformacoesMovel> ().tipoMovel == TipoMovel.MOVEL))
-		{
-			Vector3 nEuler = hit.transform.eulerAngles;
-			nEuler.x = - nEuler.x;
-			transform.eulerAngles = nEuler;
-		}
-
-		enableDrag = true;
+		/*Bounds b = GetComponentInChildren<MeshFilter>().mesh.bounds;
+        Vector3 offset = -1 * b.center;
+        p = last_p = new Vector3(offset.x / b.extents.x, offset.y / b.extents.y, offset.z / b.extents.z);
+        */
 	}
 
-	bool wasTurn = false;
+	float vertical, horizontal;
+	RaycastHit hit;
+	Vector2 mousePosition;
+
 	void DragObject ()
 	{
-		if(!isSelected)
+
+		if (!isSelected || !enabled || !enableDrag)
 			return;
 
-        ray = mainCamera.ScreenPointToRay (mousePosition);
+		mainCamera = GameObject.FindWithTag ("MainCamera").camera;
 
-        if(!Physics.Raycast (ray, out hit, Mathf.Infinity, scenarioLayer))
-        	return;
+		mousePosition = Input.mousePosition;
+		//mousePosition.x += Screen.width  * 0.0050f;
+		//mousePosition.y -= Screen.height * 0.0515f;
 
-		Vector3 moduleCenter = collider.bounds.center;
+		Ray ray = mainCamera.ScreenPointToRay (mousePosition);
 
-		Ray leftRay  = new Ray(moduleCenter, - transform.right.normalized);
-		Ray rightRay = new Ray(moduleCenter,   transform.right.normalized);
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity, scenarioLayer)) {
 
-//		Debug.DrawRay (leftRay.origin,  leftRay.direction,  Color.green);
-//		Debug.DrawRay (rightRay.origin, rightRay.direction, Color.blue);
-//		Debug.Break ();
+			if (hit.transform.tag == "Grid")
+				return;//Ignore Grid			
 
-		RaycastHit leftHit;
-		RaycastHit rightHit;
+			if (hit.transform.tag == "Parede" ||
+			    hit.transform.tag == "Chao" ||
+				hit.transform.tag == "Teto") {
 
-		isOnTheWall = (referenceTransform.tag == "Parede");
-
-		if (referenceTransform == hit.transform)
-		{
-			Vector3 vector = (hit.point - referencePoint);
-			referencePoint =  hit.point;
-
-			TipoMovel tipoMovel = GetComponent<InformacoesMovel>().tipoMovel;
-			if (tipoMovel == TipoMovel.FIXO ||
-				tipoMovel == TipoMovel.CANTOFIXO)
-			{
-				vector.y = 0;
-			}
-
-			if (halfModuleWidth < WallController.GetNearestWallDistance (transform))
-			{
-				transform.position += vector;
-			}
-			else
-			{
-				bool wasMouseMovedToRightSide = mousePosition.x > lastMousePosition.x;
-				bool wasCollidedOnLeftSide 	  = Physics.Raycast(leftRay,  out leftHit,
-																halfModuleWidth, scenarioLayer);
-				bool wasCollidedOnRightSide   = Physics.Raycast(rightRay, out rightHit,
-																halfModuleWidth, scenarioLayer);
-
-				if ( ( wasMouseMovedToRightSide && !wasCollidedOnLeftSide)  ||
-					 (!wasMouseMovedToRightSide && !wasCollidedOnRightSide) )
-				{
-					transform.position += vector;
+				/*if( p != last_p) { 
+					//Detects user input on any of the three sliders
+	               	UpdatePivot(transform.GetComponentInChildren<MeshFilter>());
+            	}*/
+				if (GetComponent<InformacoesMovel> ().tipoMovel != TipoMovel.MOVEL) {
+					transform.position = (hit.point.x * transform.parent.right) + 
+										 (hit.point.z * transform.parent.forward);
+				} else {
+					transform.position = (hit.point.x * transform.parent.right) + 
+										 (hit.point.y * transform.parent.up) + 
+										 (hit.point.z * transform.parent.forward);
 				}
+			}
+
+			//transform.position += new Vector3(0,0.2f,0);
+
+			wasDragged = true;
+		}
+	}
+
+	bool MinMax (float value, float min, float max)
+	{
+		return (value > min) && (value < max);
+	}
+
+	void UpdatePivot (MeshFilter meshFilter)
+	{
+		Vector3 diff = Vector3.Scale (meshFilter.mesh.bounds.extents, last_p - p); //Calculate difference in 3d position
+		meshFilter.transform.position -= Vector3.Scale (diff, meshFilter.transform.localScale); //Move object position
+		last_p = p;
+		if (collider) {
+			if (collider is BoxCollider) {
+				((BoxCollider)collider).center += diff;
+			} else if (collider is CapsuleCollider) {
+				((CapsuleCollider)collider).center += diff;
+			} else if (collider is SphereCollider) {
+				((SphereCollider)collider).center += diff;
 			}
 		}
-		else
-		{
-			Vector3 newPosition = hit.point;
-
-			if (!isOnTheWall)
-			{
-				if ( hit.transform.tag == "Teto" &&
-					(GetComponent<InformacoesMovel> ().tipoMovel == TipoMovel.MOVEL))
-				{
-					Vector3 nEuler = hit.transform.eulerAngles;
-					nEuler.x = - nEuler.x;
-					transform.eulerAngles = nEuler;
-				}
-			}
-			if (isOnTheWall && hit.transform.tag == "Parede")
-			{
-				transform.rotation = hit.transform.rotation;
-
-				//se não é direita é esquerda
-				if (Physics.Raycast(rightRay, out rightHit, halfModuleWidth, scenarioLayer))
-				{
-					newPosition =  hit.transform.position
-								- (transform.right.normalized * hit.transform.localScale.x)
-								+ (transform.right.normalized * halfModuleWidth);
-				}
-				else
-				{
-					newPosition =  hit.transform.position
-								- (transform.right.normalized * halfModuleWidth);
-				}
-			}
-
-			TipoMovel tipoMovel = GetComponent<InformacoesMovel>().tipoMovel;
-			if (tipoMovel == TipoMovel.FIXO ||
-				tipoMovel == TipoMovel.CANTOFIXO)
-			{
-				newPosition.y = transform.position.y;
-			}
-
-			referenceTransform = hit.transform;
-			transform.position = newPosition;
-		}
-
-		wasDragged = true;
 	}
 
 	void DropObject ()
 	{
-		enableDrag = false;
-
-		if(!isSelected || !enabled)
+		if (!isSelected || !enabled)
 			return;
 
-		if (GetComponent<InformacoesMovel>().tipoMovel != TipoMovel.MOVEL)
-		{
+		enableDrag = false;
+
+		if (GetComponent<InformacoesMovel> ().tipoMovel != TipoMovel.MOVEL) {
 			this.rigidbody.constraints = RigidbodyConstraints.FreezePositionY |
-										 RigidbodyConstraints.FreezeRotation;
-		}
-		else
-		{
+											RigidbodyConstraints.FreezeRotation;
+		} else {
 			this.rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
 		}
-
+		/*		
+		if(wasDragged){
+			transform.position -= new Vector3(0,0.2f,0);
+		}
+		*/
+		//need to refresh wasDragged 
 		wasDragged = false;
-		
-//		Invoke("VerifyGround",0.2f);
+
+		Invoke ("VerifyGround", 0.2f);
 	}
-	
-	void VerifyGround()
+
+	void VerifyGround ()
 	{
 		uint foundGround = 0;
-		
-		Vector3[] origins =  new Vector3[4]{new Vector3(this.collider.bounds.center.x - 
+
+		Vector3[] origins = new Vector3[4]{new Vector3 (this.collider.bounds.center.x - 
 			                                            this.collider.bounds.size.x / 4,
 						                             	2,
 			                                            this.collider.bounds.center.z - 
 			                                            this.collider.bounds.size.z / 4),
-											new Vector3(this.collider.bounds.center.x - 
+											new Vector3 (this.collider.bounds.center.x - 
 			                                            this.collider.bounds.size.x / 4,
 						                             	2,
 			                                            this.collider.bounds.center.z + 
 			                                            this.collider.bounds.size.z / 4),
-											new Vector3(this.collider.bounds.center.x + 
+											new Vector3 (this.collider.bounds.center.x + 
 			                                            this.collider.bounds.size.x / 4,
 						                             	2,
 			                                            this.collider.bounds.center.z - 
 			                                            this.collider.bounds.size.z / 4),
-											new Vector3(this.collider.bounds.center.x + 
+											new Vector3 (this.collider.bounds.center.x + 
 			                                            this.collider.bounds.size.x / 4,
 						                             	2,
 			                                            this.collider.bounds.center.z + 
 			                                            this.collider.bounds.size.z / 4)};
-				
 
-		foreach(Vector3 origin in origins)
-		{
-			ray = new Ray(origin, Vector3.down);
-			Debug.DrawRay(ray.origin, ray.direction * 100, Color.cyan,100);
-			if(Physics.Raycast(ray, out hit, Mathf.Infinity, LayerMask.NameToLayer("Cenario")))
-			{
-				if(hit.transform.tag == "Chao" || hit.transform.tag == "Parede")
-				{
+		Ray ray;
+		foreach (Vector3 origin in origins) {
+			ray = new Ray (origin, Vector3.down);
+//			Debug.DrawRay(ray.origin, ray.direction * 100, Color.cyan);
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity, scenarioLayer)) {
+				if (hit.transform.tag == "Chao" || hit.transform.tag == "Parede") {
 					++foundGround;
 				} else {
 //					Debug.Log(hit.transform.tag);	
 				}
-			}
-			else
-			{
+			} else {
 //				Debug.LogError ("Whata? Não pegou em nada?");
 			}
 		}
-		Debug.Log("foundGround: " + foundGround);
-		Debug.Break();
-		return;
-		
-//		Debug.Log("foundGround: " + foundGround.ToString());
-		if(foundGround != 4)
-		{
-			GameObject[] ground = GameObject.FindGameObjectsWithTag("ChaoVazio");
+//		Debug.Break();
+//		Debug.DebugBreak();
+
+		Debug.Log ("foundGround: " + foundGround.ToString ());
+		if (foundGround != 4) {
+
+			GameObject[] ground = GameObject.FindGameObjectsWithTag ("ChaoVazio");
 			GameObject nearestAvailableGround = null;
-			
+
 			float shortestDistance = float.MaxValue;
 			float distance;
-			foreach(GameObject groundPiece in ground)
-			{
-				distance = Vector3.Distance(groundPiece.transform.position,hit.point);
-				if(distance < shortestDistance)
-				{
+			foreach (GameObject groundPiece in ground) {
+				distance = Vector3.Distance (groundPiece.transform.position, hit.point);
+				if (distance < shortestDistance) {
 					shortestDistance = distance;
 					nearestAvailableGround = groundPiece;
 				}
 			}
-			
-			if (GetComponent<InformacoesMovel>().tipoMovel != TipoMovel.MOVEL)
+
+			if (GetComponent<InformacoesMovel> ().tipoMovel != TipoMovel.MOVEL)
 			{
-				this.rigidbody.position = nearestAvailableGround.transform.position;
-			} 
+				this.transform.position  = nearestAvailableGround.transform.position;
+			}
 			else 
 			{
 				Vector3 positionMobile = nearestAvailableGround.transform.position;
 				positionMobile.y = this.transform.position.y;
-				this.rigidbody.position = positionMobile;
+				this.transform.position  = positionMobile;
 			}
 		}
 	}
